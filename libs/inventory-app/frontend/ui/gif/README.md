c9c48d6f8cd6244c642f072fe98af157d53e0f01
FabianRG1990 <103287298+FabianRG1990@users.noreply.github.com>
2024-04-20 20:25:03 -0600

    feat: library gif

---
+++ libs/inventory-app/frontend/ui/gif/README.md
@@ -0,0 +1,7 @@
+# gif
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test gif` to execute the unit tests.
---
+++ libs/inventory-app/frontend/ui/gif/src/lib/gif/gif.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'fabian-rodriguez-workspace-gif',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './gif.component.html',
+  styleUrl: './gif.component.css',
+})
+export class GifComponent {}
