c9b331bda9be041b588cdb89c44a08179387c248
FabianRG1990 <103287298+FabianRG1990@users.noreply.github.com>
2024-04-22 01:22:11 -0600

    feat: gif routes

--- apps/gif-app/src/app/app.routes.ts
+++ apps/gif-app/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Route } from '@angular/router';
+import { GifComponent } from '@fabian-rodriguez-workspace/gif';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '', component: GifComponent,
+    loadChildren: () => import('@fabian-rodriguez-workspace/gif').then((r) => r.routes),
+  },
+];
+
+
+
+
